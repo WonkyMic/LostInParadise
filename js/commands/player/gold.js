2e43dbcf2b2c1aa0b7773a989996fdc6a0620304
WonkyMic <wonkymic@gmail.com>
2020-06-25 00:41:18 -0500

    couchbase has connection failure

--- js/commands/player/gold.js
+++ js/commands/player/gold.js
@@ -1,14 +1,30 @@
 const { MessageEmbed } = require("discord.js")
+const GoldMiner = require("../../managers/goldminer")
 module.exports = {
     name:"gold",
     category: "player",
     description: "Returns Player Gold Currency",
     run: async (client, message, args) => {
+        var GM = new GoldMiner(message.author.id)
+        var goldDoc = await GM.getGoldDoc()
+        if (args.length > 1) {
+            if(args[0] == "add") {
+                goldDoc.value += args[1]
+                goldDoc = GM.upsertGoldDoc(goldDoc)
+            }
+            if(args[0] == "sub") {
+                goldDoc.value -= args[1]
+                goldDoc = GM.upsertGoldDoc(goldDoc)
+            }
+        }
+        
         const embed = new MessageEmbed()
             .setColor("#F0DB4F")
-            .setDescription(`Gold: ${Math.floor(Math.random(0, 11) * 10)}`)
+            .setDescription(`Gold: ${goldDoc.value}`)
             .setFooter(message.author.username)
 
         message.channel.send(embed)
+
+        
     }
 }
