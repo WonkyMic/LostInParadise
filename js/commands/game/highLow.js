200dc2140069dda62a9044d96ccea9eb99b0aa0c
John Wimberly <johncwimberly@yahoo.com>
2020-06-25 09:04:50 -0500

    cleaner more robust high low game with winnings

--- js/commands/game/highLow.js
+++ js/commands/game/highLow.js
@@ -2,6 +2,8 @@ const { MessageEmbed } = require("discord.js")
 const sorryStringFormat = "Sorry <@%u> but you lost %s gold :("
 const winningStringFormat = "Congratulations <@%u> you did it! You win %s gold!"
 const somethingWentWrong = "Your commands didn't make sense, try again! ex : `!r <high/low> 40`"
+const _HIGH_ = "high"
+const _LOW_ = "low"
 
 
 module.exports = {
@@ -9,69 +11,59 @@ module.exports = {
     category: "game",
     description: "Plays a simple high/low game with gold bets",
     run: async (client, message, args) => {
+        let betValue
+        let returnString
+        let winnings = 0
 
-        function failure() {
+        function errorWarning() {
             const errMsg = new MessageEmbed()
             .setColor("#FF0000")
             .setDescription(somethingWentWrong)
             message.channel.send(errMsg)
         }
 
-        let betValue
-        let returnString
-        let winnings = 0
-
-        let side = args.shift().toLowerCase()
-        if(side != "high" && side != "low"){
-            failure()
-            return
+        function win() {
+            returnString += winningStringFormat
+            winnings += betValue*2
+        }
+        function lose() {
+            returnString += sorryStringFormat
+            winnings = betValue
         }
 
-        //if args exist get the next one as the bet value
-        if(Array.isArray(args) && args.length > 0 ){
-            betValue = args.shift().toLowerCase()
-        }
-        else {
-            failure()
-            return
-        }
-        //if the betValue is not a number, give error and no winnings
-        if(isNaN(betValue)){
-            failure()
-            return
-        }
+        if(Array.isArray(args) && args.length > 1 ){ //to stop processing if not enough args
+            let side = args.shift().toLowerCase()
 
-        //Do the roll and build appropriate string
-        const roll = Math.trunc(Math.random()*100) + 1
-        returnString = "You rolled a " + roll + ".\n"
+            if(side == _HIGH_ || side == _LOW_){ //must provide "low"/"high"
+                betValue = args.shift().toLowerCase()
 
-        if(side == "high"){
-            //if they bet high and succeed, double the betVal as winnings
-            if(roll > 70){
-                returnString += winningStringFormat
-                winnings += betValue*2
-            }
-            else {
-                returnString += sorryStringFormat
-                winnings = betValue
-            }
-        }
-        else if(side == "low"){
-            //if they bet low and succeed, double the betVal as winnings
-            if(roll < 30){
-                returnString += winningStringFormat
-                winnings += betValue*2
-            }
-            else {
-                returnString += sorryStringFormat
-                winnings = betValue
+                if(!isNaN(betValue)){ //if betValue IS a number
+                    const roll = Math.trunc(Math.random()*100) + 1
+                    returnString = "You rolled a " + roll + ".\n"
+                    if(side == _HIGH_){
+                        if(roll > 70){
+                            win()
+                        }
+                        else {
+                            lose()
+                        }
+                    }
+                    else if(side == _LOW_){
+                        if(roll < 30){
+                            win()
+                        }
+                        else {
+                            lose()
+                        }
+                    }
+                    returnString = returnString.replace(/\%s/g, winnings)
+                    returnString = returnString.replace(/\%u/g, message.author.id)
+
+                    message.channel.send(returnString)
+                    return
+                }
             }
         }
-        else { return }
-
-        returnString = returnString.replace(/\%s/g, winnings)
-        returnString = returnString.replace(/\%u/g, message.author.id)
-
-        message.channel.send(returnString)
+        errorWarning()
     }
 }
