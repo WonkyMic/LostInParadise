44367f2555e0570ed780f89580d200d85a56de67
John Wimberly <johncwimberly@yahoo.com>
2020-06-25 00:05:01 -0500

    added reward functionality and prettied up output for highLow game

--- js/commands/game/highLow.js
+++ js/commands/game/highLow.js
@@ -1,60 +1,77 @@
-const sorryStringFormat = "Sorry, but you lost %s tokens :("
-const winningStringFormat = "Congratulations, you did it! You win %s tokens!"
-const somethingWentWrong = "Your commands didn't make sense, try again! ex : `!high 40`"
+const { MessageEmbed } = require("discord.js")
+const sorryStringFormat = "Sorry <@%u> but you lost %s gold :("
+const winningStringFormat = "Congratulations <@%u> you did it! You win %s gold!"
+const somethingWentWrong = "Your commands didn't make sense, try again! ex : `!r <high/low> 40`"
+
 
 module.exports = {
-    name:"highLow",
+    name: "r",
     category: "game",
-    description: "todo",
+    description: "Plays a simple high/low game with gold bets",
     run: async (client, message, args) => {
 
-        console.log("--- Running highLow ---")
-        console.log("--- args : " + args + " ---")
+        function failure() {
+            const errMsg = new MessageEmbed()
+            .setColor("#FF0000")
+            .setDescription(somethingWentWrong)
+            message.channel.send(errMsg)
+        }
 
         let betValue
+        let returnString
         let winnings = 0
+
+        let side = args.shift().toLowerCase()
+        if(side != "high" && side != "low"){
+            failure()
+            return
+        }
 
         //if args exist get the next one as the bet value
         if(Array.isArray(args) && args.length > 0 ){
             betValue = args.shift().toLowerCase()
         }
         else {
-            return somethingWentWrong
+            failure()
+            return
         }
         //if the betValue is not a number, give error and no winnings
         if(isNaN(betValue)){
-            return somethingWentWrong
+            failure()
+            return
         }
 
         //Do the roll and build appropriate string
         const roll = Math.trunc(Math.random()*100) + 1
-        var returnString = "You rolled a " + roll + ".\n"
+        returnString = "You rolled a " + roll + ".\n"
 
-        if(cmd == "high"){
+        if(side == "high"){
             //if they bet high and succeed, double the betVal as winnings
             if(roll > 70){
                 returnString += winningStringFormat
-                winnings += betVal*2
+                winnings += betValue*2
             }
             else {
                 returnString += sorryStringFormat
-                winnings = betVal
+                winnings = betValue
             }
         }
-        else if(cmd == "low"){
+        else if(side == "low"){
             //if they bet low and succeed, double the betVal as winnings
             if(roll < 30){
                 returnString += winningStringFormat
-                winnings += betVal*2
+                winnings += betValue*2
             }
             else {
                 returnString += sorryStringFormat
-                winnings = betVal
+                winnings = betValue
             }
         }
         else { return }
 
         returnString = returnString.replace(/\%s/g, winnings)
-        return returnString
+        returnString = returnString.replace(/\%u/g, message.author.id)
+
+        message.channel.send(returnString)
     }
 }
--- js/commands/player/gold.js
+++ js/commands/player/gold.js
@@ -1,9 +1,11 @@
 const { MessageEmbed } = require("discord.js")
 module.exports = {
-    name:"gold",
+    name: "gold",
     category: "player",
     description: "Returns Player Gold Currency",
     run: async (client, message, args) => {
+        console.log("--- Running gold ---")
+        console.log("--- args : " + args + " ---")
         const embed = new MessageEmbed()
             .setColor("#F0DB4F")
             .setDescription(`Gold: ${Math.floor(Math.random(0, 11) * 10)}`)
